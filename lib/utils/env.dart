7dbd8fe4d3d30a7a836e8387e986d56c28bdaf94
Choy050823 <choymh23@gmail.com>
2025-06-23 11:07:22 +0800

    Updated new AWS EC2 Backend

--- lib/utils/env.dart
+++ lib/utils/env.dart
@@ -1,7 +1,7 @@
 const environment = {
   // local (dev)
-  'api_url': "http://localhost:8080"
+  // 'api_url': "http://localhost:8080"
 
   // cloud (production)
-  // 'api_url': "https://wanderverse-server.onrender.com"
+  'api_url': "https://api.wanderverse-service.org"
 };
