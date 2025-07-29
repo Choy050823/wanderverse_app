9697b8857d88a253786bcdb33e939254b7b29de2
Choy050823 <choymh23@gmail.com>
2025-07-29 09:02:00 +0800

    done checking files, can go prod

--- lib/screens/post-sharing/homeScreen.dart
+++ lib/screens/post-sharing/homeScreen.dart
@@ -350,8 +350,10 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
           ],
           const SizedBox(height: 24),
           ElevatedButton(
-            onPressed: () =>
-                ref.read(sharingPostsProvider.notifier).refreshPosts(),
+            onPressed: () {
+              ref.read(sharingPostsProvider.notifier).refreshPosts();
+              ref.read(sharingPostsProvider.notifier).getRecommendedPosts();
+            },
             child: const Text("Refresh"),
           ),
         ],
